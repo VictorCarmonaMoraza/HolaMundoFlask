da0dc2f718aa201b8703070fafe0c8b2ba7f337e
Victor <VictorCarmona9035@hotmail.com>
2023-05-10 23:33:09 +0200

    3-Routing y pasode parametros con Flask

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 def inicio():
     app.logger.info(f'Entramos al path {request.path}')
     return '<h1>Hello Mundo desde Flask2!</h1>'
+
+
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    return f'Saludos {nombre.upper()}'
+
+@app.route('/edad/<int:edad>')
+def mostrar_edad(edad):
+        if edad >25:
+            return f'Tu edad es: {edad + 5}'
+        else:
+            return f'Tu edad es: {edad +1}'
