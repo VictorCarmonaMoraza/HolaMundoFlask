7667c1c80949e8a514e0fd3772b2cc904af7db7c
Victor <VictorCarmona9035@hotmail.com>
2023-05-10 22:51:56 +0200

    2-Uso de Logging en Flask

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 #http://localhost:5000
 @app.route('/')
 def inicio():
+    app.logger.info(f'Entramos al path {request.path}')
     return '<h1>Hello Mundo desde Flask2!</h1>'
